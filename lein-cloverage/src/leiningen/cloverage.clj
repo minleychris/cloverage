08d05b5986eec300a7fabeb421eb7118504b6369
Chris Wilson <chris@cjetech.co.uk>
2014-09-12 16:17:52 +0200

    Adding in leiningen config

--- lein-cloverage/src/leiningen/cloverage.clj
+++ lein-cloverage/src/leiningen/cloverage.clj
@@ -8,6 +8,14 @@
 (defn get-lib-version []
   (or (System/getenv "CLOVERAGE_VERSION") "RELEASE"))
 
+(defn project-file-to-commandline-args [data]
+  (reduce (fn [r [k v]]
+            (if (sequential? v)
+              (reduce (fn [l val] (conj l (str "--" (name k)) val)) [] v)
+              (concat r (list (str "--" (name k)) v))))
+          '()
+          data))
+
 (defn cloverage
   "Run code coverage on the project.
 
@@ -15,10 +23,13 @@
   Specify -o OUTPUTDIR for output directory, for other options see cloverage."
   [project & args]
   (let [source-namespaces (ns-names-for-dirs (:source-paths project))
-        test-namespace    (ns-names-for-dirs (:test-paths project))]
+        test-namespace    (ns-names-for-dirs (:test-paths project))
+        cloverage-args    (project-file-to-commandline-args (:cloverage project))]
+
     (apply run/run (update-in project [:dependencies]
                               conj    ['cloverage (get-lib-version)])
            "-m" "cloverage.coverage"
            (concat (mapcat  #(list "-x" %) test-namespace)
                    args
+                   cloverage-args
                    source-namespaces))))
